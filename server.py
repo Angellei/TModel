5d6ef9b535003e35e6587d0914b15694eb330395
Angellei <36525178+Angellei@users.noreply.github.com>
2019-03-19 23:10:55 -0700

    Add files

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, jsonify, Response
+import pickle
+import pandas as pd
+
+#create the app object that wil route our calls
+app = Flask(__name__)
+
+#get method is to get the data from website
+#route = homepage in the future, may use '/mpg'
+@app.route('/', methods=["GET"])
+def home():
+    return render_template('home.html')
+
+@app.route('/mpg', methods=["GET"])
+def mpg():
+    return render_template('mpg.html')
+model = pickle.load(open('linreg.p', 'rb'))
+
+@app.route('/inference', methods=["POST"])
+def inference():
+    req = request.get_json()
+    print(req)
+    c, h,w = req['cylinders'],req['horsepower'],req['weight']
+    prediction = list(model.predict([[c,h,w]]))
+    return jsonify({'c':c, 'h':h, 'w':w, 'prediction':prediction[0]})
+
+@app.route('/plot', methods=["GET"])
+def plot():
+    df = pd.read_csv('cars.csv')
+    data = list(zip(df.mpg, df.weight))
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3333,debug=True)
